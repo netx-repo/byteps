1dc74bbec4d1cf3aeea439cae18de9592c83d080
rivendile <908636821@qq.com>
2020-12-22 11:54:45 +0000

    update

--- bytescheduler/examples/mxnet-image-classification/run_dist_server.sh
+++ bytescheduler/examples/mxnet-image-classification/run_dist_server.sh
@@ -1,1 +1,1 @@
-./dist_server.sh  2 2 0 0 0  python train_imagenet.py --network resnet --num-layers 50 --benchmark 1 --kv-store dist_sync --batch-size 32 --disp-batches 2 --num-examples 10 --num-epochs 1 --gpus 0 --role server
+./dist_server.sh  2 2 0 0 0  python train_imagenet.py --network resnet --num-layers 50 --benchmark 1 --kv-store dist_sync --batch-size 32 --disp-batches 2 --num-examples 384 --num-epochs 1 --gpus 0 --role server
