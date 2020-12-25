a6d9f49454f03b04bb3a69d17d329482e383e1d5
rivendile <908636821@qq.com>
2020-12-25 01:37:01 +0000

    update

--- bytescheduler/examples/mxnet-image-classification/run.sh
+++ bytescheduler/examples/mxnet-image-classification/run.sh
@@ -1,3 +1,3 @@
 # ./local.sh 1 1 python -m trace --ignore-dir=/usr/lib/python2.7 --trace train_imagenet.py --network vgg --num-layers 16 --benchmark 1 --kv-store dist_sync --batch-size 2 --disp-batches 10 --num-examples 22 --num-epochs 1 --gpus 0 >trace_bytescheduler_test.txt
 # dmlc_num_server, dmlc_num_worker, ps_slicer, use_bytescheduler, bytescheduler_queue_type, kvstore_map_kind, kvstore_map_model
- ./local.sh 4 1 0 0 0 0 0 python train_imagenet.py --network resnet --num-layers 50 --benchmark 1 --kv-store dist_sync --batch-size 2 --disp-batches 1 --num-examples 6 --num-epochs 1 --gpus 0
+ ./local.sh 4 1 0 0 0 0 0 python train_imagenet.py --network resnet --num-layers 50 --benchmark 1 --kv-store dist_sync --batch-size 32 --disp-batches 100 --num-examples 26880 --num-epochs 1 --gpus 0
