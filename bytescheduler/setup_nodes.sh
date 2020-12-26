96834223186a18ccb7bb041337414b96a7112ccc
rivendile <908636821@qq.com>
2020-12-26 09:06:05 +0000

    update

--- bytescheduler/setup_nodes.sh
+++ bytescheduler/setup_nodes.sh
@@ -15,11 +15,11 @@ ssh -i ~/byteps/bytescheduler/bytescheduler-20201004.pem ubuntu@172.31.94.121 "e
 # setup docker and connect to container
 # mxnet ps t4 
 docker login -u rivendile -p zhyh19980824
-docker pull rivendile/bsc-mxnet-ps-ssh-t4
-nvidia-docker run -it --gpus all --ipc=host --name mlnet-analysis --network host -v /home/ubuntu/byteps:/home/cluster/byteps --detach 4f99085b602b
+docker pull rivendile/bsc-mxnet-ps-ssh
+nvidia-docker run -it --gpus all --ipc=host --name mlnet-analysis --network host -v /home/ubuntu/byteps:/home/cluster/byteps --detach bd1b3ec517f2
 docker_container_id=$(docker ps -aqf "name=mlnet-analysis")
-docker cp $docker_container_id:/home/cluster/.ssh ~/mxnet_ps_ssh_t4
-ssh -i ~/mxnet_ps_ssh_t4/id_rsa -p 2022 cluster@localhost
+docker cp $docker_container_id:/home/cluster/.ssh ~/mxnet_ps_ssh
+ssh -i ~/mxnet_ps_ssh/id_rsa -p 2022 cluster@localhost
 cd ~/byteps/bytescheduler/examples/mxnet-image-classification
 git config user.email "908636821@qq.com"
 git config user.name "rivendile"
