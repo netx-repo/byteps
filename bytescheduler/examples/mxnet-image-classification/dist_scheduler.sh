82e9f33c8d9648b9f05622f489ec8f8bc8221f05
Yihao Zhao <908636821@qq.com>
2020-12-26 17:37:34 +0800

    Update dist_scheduler.sh

--- bytescheduler/examples/mxnet-image-classification/dist_scheduler.sh
+++ bytescheduler/examples/mxnet-image-classification/dist_scheduler.sh
@@ -35,7 +35,7 @@ shift
 arg="$@"
 
 # start the scheduler
-export DMLC_PS_ROOT_URI='172.31.16.164'
+export DMLC_PS_ROOT_URI='172.31.26.226'
 export DMLC_PS_ROOT_PORT=8000
 export DMLC_ROLE='scheduler'
 #nvprof -o bs_scheduler_profile.nvvp ${bin} ${arg} &
