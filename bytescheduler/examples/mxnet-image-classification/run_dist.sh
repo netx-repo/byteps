c869e5a857b469fcad9ddd2400769d47c9d0750c
rivendile <908636821@qq.com>
2020-12-31 05:12:39 +0000

    update

--- bytescheduler/examples/mxnet-image-classification/run_dist.sh
+++ bytescheduler/examples/mxnet-image-classification/run_dist.sh
@@ -1,6 +1,6 @@
 export dmlc_num_server=1
 export dmlc_num_worker=1
-export use_bytescheduler=0
+export use_bytescheduler=1
 export bytescheduler_queue_type=0
 export bytescheduler_partition=8000000
 export dmlc_ps_root_uri='172.31.90.60'
@@ -10,6 +10,6 @@ DMLC_ROLE='scheduler' DMLC_NUM_SERVER=$dmlc_num_server DMLC_NUM_WORKER=$dmlc_num
 
 DMLC_ROLE='server' DMLC_NUM_SERVER=$dmlc_num_server DMLC_NUM_WORKER=$dmlc_num_worker USE_BYTESCHEDULER=$use_bytescheduler BYTESCHEDULER_QUEUE_TYPE=$bytescheduler_queue_type BYTESCHEDULER_PARTITION=$bytescheduler_partition DMLC_PS_ROOT_URI=$dmlc_ps_root_uri DMLC_PS_ROOT_PORT=8000 BYTESCHEDULER_CREDIT_TUNING=0 BYTESCHEDULER_PARTITION_TUNING=0 PS_SLICER=0 KVSTORE_MAP_KIND=0 KVSTORE_MAP_MODEL=1 $COMMAND &
 
-ssh cluster@172.31.89.77 "cd /home/cluster/byteps/bytescheduler/examples/mxnet-image-classification;DMLC_ROLE='worker' DMLC_NUM_SERVER=$dmlc_num_server DMLC_NUM_WORKER=$dmlc_num_worker USE_BYTESCHEDULER=$use_bytescheduler BYTESCHEDULER_QUEUE_TYPE=$bytescheduler_queue_type BYTESCHEDULER_PARTITION=$bytescheduler_partition DMLC_PS_ROOT_URI=$dmlc_ps_root_uri DMLC_PS_ROOT_PORT=8000 BYTESCHEDULER_CREDIT_TUNING=0 BYTESCHEDULER_PARTITION_TUNING=0 PS_SLICER=0 KVSTORE_MAP_KIND=0 KVSTORE_MAP_MODEL=1 $COMMAND >test.txt 2>&1" &
+ssh cluster@172.31.89.77 "cd /home/cluster/byteps/bytescheduler/examples/mxnet-image-classification;DMLC_ROLE='worker' DMLC_NUM_SERVER=$dmlc_num_server DMLC_NUM_WORKER=$dmlc_num_worker USE_BYTESCHEDULER=$use_bytescheduler BYTESCHEDULER_QUEUE_TYPE=$bytescheduler_queue_type BYTESCHEDULER_PARTITION=$bytescheduler_partition DMLC_PS_ROOT_URI=$dmlc_ps_root_uri DMLC_PS_ROOT_PORT=8000 BYTESCHEDULER_CREDIT_TUNING=0 BYTESCHEDULER_PARTITION_TUNING=0 PS_SLICER=0 KVSTORE_MAP_KIND=0 KVSTORE_MAP_MODEL=1 $COMMAND >test.txt 2>&1 &"
 
 #./dist_scheduler.sh 1 1 0 0 0 1 8000000 python train_imagenet.py --network vgg --num-layers 16 --benchmark 1 --kv-store dist_sync --batch-size 256 --disp-batches 10 --num-examples 28416 --num-epochs 1 --gpus 0,1,2,3,4,5,6,7 --role scheduler
