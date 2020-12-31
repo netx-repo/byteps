f09bdb7936407277ef8ee54477f81803a0454129
rivendile <908636821@qq.com>
2020-12-31 02:13:02 +0000

    update

--- bytescheduler/setup_nodes.sh
+++ bytescheduler/setup_nodes.sh
@@ -17,10 +17,10 @@ ssh -i ~/byteps/bytescheduler/bytescheduler-20201004.pem ubuntu@172.31.90.60 "ec
 
 cd ~/
 # setup docker and connect to container
-# mxnet ps t4 
+# mxnet ps ssh t4 myps
 docker login -u rivendile -p zhyh19980824
-docker pull rivendile/bsc-mxnet-ps-ssh-t4-myps-pip:ps-update
-nvidia-docker run -it --gpus all --ipc=host --name mlnet-analysis --network host -v /home/ubuntu/byteps:/home/cluster/byteps --detach 0fcd2b318501
+docker pull rivendile/bsc-mxnet-ps-ssh
+nvidia-docker run -it --gpus all --ipc=host --name mlnet-analysis --network host -v /home/ubuntu/byteps:/home/cluster/byteps --detach bd1b3ec517f2
 docker_container_id=$(docker ps -aqf "name=mlnet-analysis")
 docker cp $docker_container_id:/home/cluster/.ssh ~/mlnet_analysis_ssh
 ssh -i ~/mlnet_analysis_ssh/id_rsa -p 2022 cluster@localhost
