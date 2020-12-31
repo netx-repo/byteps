c15de0fcceb0332c396111dcf6d71f0b0d0db172
rivendile <908636821@qq.com>
2020-12-31 06:38:38 +0000

    update

--- bytescheduler/examples/mxnet-image-classification/run_dist.sh
+++ bytescheduler/examples/mxnet-image-classification/run_dist.sh
@@ -1,10 +1,10 @@
-export dmlc_num_server=8
-export dmlc_num_worker=8
-export use_bytescheduler=0
-export bytescheduler_queue_type=0
-export bytescheduler_partition=8000000
+export dmlc_num_server=1
+export dmlc_num_worker=1
+export use_bytescheduler=1
+export bytescheduler_queue_type=2
+export bytescheduler_partition=160000000
 export dmlc_ps_root_uri='172.31.80.96'
-export COMMAND='python train_imagenet.py --network vgg --num-layers 16 --kv-store dist_sync_device --batch-size 32 --disp-batches 10 --num-epochs 1 --gpus 0 --benchmark 0 --data-train /home/cluster/data/data_train.rec --data-train-idx /home/cluster/data/data_train.idx --data-val /home/cluster/data/data_val.rec --data-val-idx /home/cluster/data/data_val.idx --data-nthreads 8'
+export COMMAND='python train_imagenet.py --network resnet --num-layers 50 --kv-store dist_sync_device --batch-size 32 --disp-batches 10 --num-epochs 1 --gpus 0 --benchmark 0 --data-train /home/cluster/data/data_train.rec --data-train-idx /home/cluster/data/data_train.idx --data-val /home/cluster/data/data_val.rec --data-val-idx /home/cluster/data/data_val.idx --data-nthreads 8'
 
 # scheduler
 DMLC_ROLE='scheduler' DMLC_NUM_SERVER=$dmlc_num_server DMLC_NUM_WORKER=$dmlc_num_worker USE_BYTESCHEDULER=$use_bytescheduler BYTESCHEDULER_QUEUE_TYPE=$bytescheduler_queue_type BYTESCHEDULER_PARTITION=$bytescheduler_partition DMLC_PS_ROOT_URI=$dmlc_ps_root_uri DMLC_PS_ROOT_PORT=8000 BYTESCHEDULER_CREDIT_TUNING=0 BYTESCHEDULER_PARTITION_TUNING=0 PS_SLICER=0 KVSTORE_MAP_KIND=0 KVSTORE_MAP_MODEL=1 $COMMAND &
