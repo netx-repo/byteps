ebf7604d362d17c05a1142b3871118aa33dfeeed
Ubuntu <ubuntu@ip-172-31-80-30.ec2.internal>
2021-01-07 07:17:23 +0000

    upate

--- setup_nodes_byteps_pytorch.sh
+++ setup_nodes_byteps_pytorch.sh
@@ -29,7 +29,7 @@ cd ~/
 # mxnet ps ssh t4 myps
 docker login -u rivendile -p zhyh19980824
 docker pull rivendile/byteps-pytorch
-nvidia-docker run -it --gpus all --ipc=host --name mlnet-analysis-byteps-pytorch --network host -v /home/ubuntu/byteps:/home/cluster/byteps --detach 70912422872f
+nvidia-docker run -it --gpus all --ipc=host --name mlnet-analysis-byteps-pytorch --network host -v /home/ubuntu/byteps:/home/cluster/byteps --detach 535ec11ae861
 docker_container_id=$(docker ps -aqf "name=mlnet-analysis-byteps-pytorch")
 docker cp $docker_container_id:/home/cluster/.ssh ~/mlnet_analysis_byteps_pytorch_ssh
 ssh -i ~/mlnet_analysis_byteps_pytorch_ssh/id_rsa -p 2022 cluster@localhost
