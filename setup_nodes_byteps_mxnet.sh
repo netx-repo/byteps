b5ed080e485bbe521d0822f92c25d35f91ba23e6
rivendile <908636821@qq.com>
2021-01-08 06:39:07 +0000

    update

--- setup_nodes_byteps_mxnet.sh
+++ setup_nodes_byteps_mxnet.sh
@@ -29,7 +29,7 @@ cd ~/
 # mxnet ps ssh t4 myps
 docker login -u rivendile -p zhyh19980824
 docker pull rivendile/byteps-mxnet
-nvidia-docker run -it --gpus all --ipc=host --name mlnet-analysis-byteps-mxnet --network host -v /home/ubuntu/byteps:/home/cluster/byteps --detach b3a44935d793
+nvidia-docker run -it --gpus all --ipc=host --name mlnet-analysis-byteps-mxnet --network host -v /home/ubuntu/byteps:/home/cluster/byteps --detach 3fe34daac76b
 docker_container_id=$(docker ps -aqf "name=mlnet-analysis-byteps-mxnet")
 docker cp $docker_container_id:/home/cluster/.ssh ~/mlnet_analysis_byteps_mxnet_ssh
 ssh -i ~/mlnet_analysis_byteps_mxnet_ssh/id_rsa -p 2022 cluster@localhost
