80981b139e198d226de4478735316039989c205c
Ubuntu <ubuntu@ip-172-31-93-214.ec2.internal>
2021-01-11 03:25:53 +0000

    update

--- bytescheduler/examples/mxnet-image-classification/run_partition_dist.sh
+++ bytescheduler/examples/mxnet-image-classification/run_partition_dist.sh
@@ -11,4 +11,4 @@ export PS_VERBOSE=1
 export BYTESCHEDULER_ROOT_IP=localhost
 export BYTESCHEDULER_ROOT_PORT=8000
 
-horovodrun -np 8 -H localhost:4,3.93.217.10:4 -p 2022 python train_imagenet_horovod.py --network vgg --num-layers 16 --kv-store dist_sync_device --batch-size 32 --disp-batches 10 --num-epochs 1 --gpus 0 --benchmark 0 --data-train ~/data/data_train.rec --data-train-idx ~/data/data_train.idx --data-val ~/data/data_val.rec --data-val-idx ~/data/data_val.idx --data-nthreads 8
+horovodrun -np 2 -H localhost:1,35.171.164.27:1 python train_imagenet_horovod.py --network vgg --num-layers 16 --kv-store dist_sync_device --batch-size 32 --disp-batches 10 --num-epochs 1 --gpus 0 --benchmark 1 --num-examples 16352 #--data-train ~/data/data_train.rec --data-train-idx ~/data/data_train.idx --data-val ~/data/data_val.rec --data-val-idx ~/data/data_val.idx --data-nthreads 8
