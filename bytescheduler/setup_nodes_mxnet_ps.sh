5eb001d8eb38da506b5734c93c3b0bdc3a19d825
ZYCCC927 <61256396+ZYCCC927@users.noreply.github.com>
2021-03-16 11:03:14 +0800

    Update setup_nodes_mxnet_ps.sh

--- bytescheduler/setup_nodes_mxnet_ps.sh
+++ bytescheduler/setup_nodes_mxnet_ps.sh
@@ -13,7 +13,7 @@ chmod 600 ~/.ssh/config
 chmod 600 ~/byteps/bytescheduler/bytescheduler-20201004.pem
 chmod 600 ~/byteps/bytescheduler/bytescheduler-0105.pem
 id2translate=$(cat ~/.ssh/id_rsa.pub)
-ssh -i ~/byteps/bytescheduler/bytescheduler-0105.pem ubuntu@172.31.15.181 "echo $id2translate >>~/.ssh/authorized_keys"
+ssh -i ~/byteps/bytescheduler/bytescheduler-0105.pem ubuntu@54.205.195.230 "echo $id2translate >>~/.ssh/authorized_keys"
 #ssh -i ~/byteps/bytescheduler/bytescheduler-0105.pem ubuntu@172.31.65.113 "echo $id2translate >>~/.ssh/authorized_keys"
 #ssh -i ~/byteps/bytescheduler/bytescheduler-0105.pem ubuntu@172.31.69.146 "echo $id2translate >>~/.ssh/authorized_keys"
 #ssh -i ~/byteps/bytescheduler/bytescheduler-0105.pem ubuntu@172.31.76.130 "echo $id2translate >>~/.ssh/authorized_keys"
@@ -33,11 +33,11 @@ ssh -i ~/byteps/bytescheduler/bytescheduler-0105.pem ubuntu@172.31.15.181 "echo 
 cd ~/
 # setup docker and connect to container
 # mxnet ps ssh t4 myps
-#docker login -u rivendile -p zhyh19980824
-docker login -u zycccc -p 314159dos
-#docker pull rivendile/bsc-mxnet-ps-ssh
-docker pull zycccc/bsc-mxnet-py3
-nvidia-docker run -it --gpus all --ipc=host --name mlnet-analysis --network host -v /home/ubuntu/byteps:/home/cluster/byteps --detach zycccc/bsc-mxnet-py3
+docker login -u rivendile -p zhyh19980824
+#docker login -u zycccc -p 314159dos
+docker pull rivendile/bsc-mxnet-ps-ssh
+#docker pull zycccc/bsc-mxnet-py3
+nvidia-docker run -it --gpus all --ipc=host --name mlnet-analysis --network host -v /home/ubuntu/byteps:/home/cluster/byteps --detach rivendile/bsc-mxnet-ps-ssh
 docker_container_id=$(docker ps -aqf "name=mlnet-analysis")
 docker cp $docker_container_id:/home/cluster/.ssh ~/mlnet_analysis_ssh
 ssh -i ~/mlnet_analysis_ssh/id_rsa -p 2022 cluster@localhost
