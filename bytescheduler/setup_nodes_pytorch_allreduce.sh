e81a830d458d145eddcd1dac62e9398f0be191a0
ZYCCC927 <61256396+ZYCCC927@users.noreply.github.com>
2021-04-06 14:32:50 +0800

    Update setup_nodes_pytorch_allreduce.sh

--- bytescheduler/setup_nodes_pytorch_allreduce.sh
+++ bytescheduler/setup_nodes_pytorch_allreduce.sh
@@ -10,10 +10,10 @@ echo "    StrictHostKeyChecking no" >>~/.ssh/config
 ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 chmod 600 ~/.ssh/config
-chmod 600 ~/byteps/bytescheduler/bytescheduler-20201004.pem
+chmod 600 ~/byteps/bytescheduler/bytescheduler-0105.pem
 id2translate=$(cat ~/.ssh/id_rsa.pub)
-ssh -i ~/byteps/bytescheduler/bytescheduler-20201004.pem ubuntu@172.31.93.214 "echo $id2translate >>~/.ssh/authorized_keys"
-ssh -i ~/byteps/bytescheduler/bytescheduler-20201004.pem ubuntu@172.31.90.237 "echo $id2translate >>~/.ssh/authorized_keys"
+ssh -i ~/byteps/bytescheduler/bytescheduler-0105.pem ubuntu@172.31.93.214 "echo $id2translate >>~/.ssh/authorized_keys"
+ssh -i ~/byteps/bytescheduler/bytescheduler-0105.pem ubuntu@172.31.90.237 "echo $id2translate >>~/.ssh/authorized_keys"
 
 cd ~/
 # setup docker and connect to container
