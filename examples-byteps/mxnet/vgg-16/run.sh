48d5b254d3ca6afbaa293f57555c26e816432508
ZYCCC927 <61256396+ZYCCC927@users.noreply.github.com>
2021-04-26 10:28:59 +0800

    Update run.sh

--- examples-byteps/mxnet/vgg-16/run.sh
+++ examples-byteps/mxnet/vgg-16/run.sh
@@ -14,4 +14,4 @@ export BYTEPS_SERVER_ENABLE_SCHEDULE=1
 export BYTEPS_SCHEDULING_CREDIT=8000000
 export BYTEPS_PARTITION_BYTES=4096000
 # you can also test other networks (e.g., ResNet) with different number of layers
-bpslaunch python3 train_imagenet.py --network vgg --num-layers 16 --batch-size 32 --benchmark 1 --num-examples 3232 --num-epochs 1 --disp-batches 10 $@
+bpslaunch python3 train_imagenet.py --network vgg --num-layers 16 --batch-size 32 --benchmark 1 --num-examples 3232 --num-epochs 1 --disp-batches 10 >/home/cluster/byteps/examples-byteps/mxnet/vgg-16/test.txt 2>&1 $@
