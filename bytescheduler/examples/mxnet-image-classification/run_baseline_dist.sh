fc88f930be45046459e3c4cbe32c664f6a9f1e3b
ZYCCC927 <61256396+ZYCCC927@users.noreply.github.com>
2021-05-17 01:17:10 +0800

    Update run_baseline_dist.sh

--- bytescheduler/examples/mxnet-image-classification/run_baseline_dist.sh
+++ bytescheduler/examples/mxnet-image-classification/run_baseline_dist.sh
@@ -9,5 +9,5 @@ export BYTESCHEDULER_PARTITION_TUNING=0
 # export BYTESCHEDULER_DEBUG=1
 export PS_VERBOSE=1
 
-horovodrun -np 2 -H localhost:1,35.171.164.27:1 python train_imagenet_horovod.py --network vgg --num-layers 16 --kv-store dist_sync_device --batch-size 32 --disp-batches 10 --num-epochs 1 --gpus 0 --benchmark 1 --num-examples 16352 #--data-train ~/data/data_train.rec --data-train-idx ~/data/data_train.idx --data-val ~/data/data_val.rec --data-val-idx ~/data/data_val.idx --data-nthreads 8
+horovodrun -np 2 -H localhost:1,172.31.42.19:1 python train_imagenet_horovod.py --network vgg --num-layers 16 --kv-store dist_sync_device --batch-size 32 --disp-batches 10 --num-epochs 1 --gpus 0 --benchmark 1 --num-examples 16352 #--data-train ~/data/data_train.rec --data-train-idx ~/data/data_train.idx --data-val ~/data/data_val.rec --data-val-idx ~/data/data_val.idx --data-nthreads 8
 
