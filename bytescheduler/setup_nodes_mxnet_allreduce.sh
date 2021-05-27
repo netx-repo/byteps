d8d9470964f61f22e873352db4e3f9832b220ebd
ZYCCC927 <61256396+ZYCCC927@users.noreply.github.com>
2021-05-27 21:50:26 +0800

    Update setup_nodes_mxnet_allreduce.sh

--- bytescheduler/setup_nodes_mxnet_allreduce.sh
+++ bytescheduler/setup_nodes_mxnet_allreduce.sh
@@ -12,15 +12,13 @@ chmod 600 ~/.ssh/id_rsa
 chmod 600 ~/.ssh/config
 chmod 600 ~/byteps/bytescheduler/bytescheduler-0105.pem
 id2translate=$(cat ~/.ssh/id_rsa.pub)
-ssh -i ~/byteps/bytescheduler/bytescheduler-0105.pem ubuntu@172.31.73.105	"echo $id2translate >>~/.ssh/authorized_keys"
-ssh -i ~/byteps/bytescheduler/bytescheduler-0105.pem ubuntu@172.31.75.197 "echo $id2translate >>~/.ssh/authorized_keys"
+ssh -i ~/byteps/bytescheduler/bytescheduler-0105.pem ubuntu@172.31.73.161 "echo $id2translate >>~/.ssh/authorized_keys"
+ssh -i ~/byteps/bytescheduler/bytescheduler-0105.pem ubuntu@172.31.71.216 "echo $id2translate >>~/.ssh/authorized_keys"
 
 cd ~/
 # setup docker and connect to container
-# mxnet ps ssh t4 myps
-docker login -u rivendile -p zhyh19980824
-docker pull rivendile/bsc-mxnet-horovod-ssh
-nvidia-docker run -it --gpus all --ipc=host --name mlnet-analysis-mxnet-horovod --network host -v /home/ubuntu/byteps:/home/cluster/byteps --detach 59c421590158
+docker pull zycccc/horovod-mxnet-aws
+nvidia-docker run -it --gpus all --ipc=host --name mlnet-analysis-mxnet-horovod --network host -v /home/ubuntu/byteps:/home/cluster/byteps --detach zycccc/horovod-mxnet-aws
 docker_container_id=$(docker ps -aqf "name=mlnet-analysis-mxnet-horovod")
 docker cp $docker_container_id:/home/cluster/.ssh ~/mlnet_analysis_mxnet_horovod_ssh
 ssh -i ~/mlnet_analysis_mxnet_horovod_ssh/id_rsa -p 2022 cluster@localhost
